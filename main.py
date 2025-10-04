eed37a39fc152cfa417967dcb4711153d428ab10
Jakub Koláček <jakub@kolacek.dev>
2025-10-04 13:30:28 +0200

    Add src to pythonpath, add argument parser

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import asyncio
 import logging
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
 from common.logger_config import setup_logging
 
 logger = logging.getLogger(__name__)
 
 
+def parse_arguments():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="LightBulbFlow Application")
+    parser.add_argument('-c', '--config', type=str, default='config.yaml', help='Path to the configuration file')
+    return parser.parse_args()
+
+
 async def main():
     setup_logging()
+    parse_arguments()
     logger.info("Starting LightBulbFlow application")
     pass
 
